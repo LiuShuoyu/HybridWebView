a7da6ad70b8b11ba9f369ded95f4c44f4e96df84
liushuoyu <liushuoyu@medlinker.com>
2017-08-10 16:56:58 +0800

    0.1.0

---
+++ HybridWebView/Classes/HybridView.swift
@@ -0,0 +1,21 @@
+//
+//  HybridView.swift
+//  Pods
+//
+//  Created by liushuoyu on 2017/8/10.
+//
+//
+
+import UIKit
+
+class HybridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
